cf31463bb7e266d3771171a0f0b486a81a40989b
kv <krisviceral@gmail.com>
2015-06-15 22:14:15 -0400

    Fixed initial loading. An index page now shows up (had to change component scan to check other packages)

--- src/main/java/log/LogAnalyticsApplication.java
+++ src/main/java/log/LogAnalyticsApplication.java
@@ -2,19 +2,11 @@ package log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@Controller
+@ComponentScan({"controller","resource"})
 public class LogAnalyticsApplication {
-
-    @ResponseBody
-    @RequestMapping("/")
-    public String helloWorld(){
-        return "Hello World";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LogAnalyticsApplication.class, args);
